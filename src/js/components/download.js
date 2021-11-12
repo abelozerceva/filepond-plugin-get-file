8575144f29f569dc67a44f78c07be560f4b24d64
Anastasia Belozerceva <belozerceva.a@ya.ru>
2021-11-12 12:54:22 +0300

    Add patch for filepond-plugin-get-file plugin for copy download link

--- src/js/components/download.js
+++ src/js/components/download.js
@@ -6,37 +6,22 @@ export const registerDownloadComponent = (item, el, labelButtonDownload, allowDo
           downloadIcon = getDownloadIcon(labelButtonDownload);
 
     info.prepend(downloadIcon);
-    downloadIcon.addEventListener("click", () => downloadFile(item, allowDownloadByUrl));
 }
 
 /**
  * Generates the download icon
  */
 export const getDownloadIcon = (labelButtonDownload) => {
-    let icon = document.createElement('span');
-    icon.className = 'filepond--download-icon';
-    icon.title = labelButtonDownload;
-    return icon;
-}
-
-/**
- * Triggers the actual download of the uploaded file
- */
-export const downloadFile = (item, allowDownloadByUrl) => {
-    // if client want to download file from remote server
-    if(allowDownloadByUrl && item.getMetadata('url')) {
-        location.href = item.getMetadata('url'); // full path to remote server is stored in metadata with key 'url'
-      } else {
-        // create a temporary hyperlink to force the browser to download the file
-        const a = document.createElement("a");
-        const url = window.URL.createObjectURL(item.file);
-        document.body.appendChild(a);
-        a.style.display = 'none';
-        a.href = url;
-        a.download = item.file.name;
-        a.click();
-
-        window.URL.revokeObjectURL(url);
-        a.remove();
-    }
+	let link = document.createElement('a');
+	let icon = document.createElement('span');
+	icon.className = 'filepond--download-icon';
+	icon.title = labelButtonDownload;
+	if (item.getMetadata('url')) {
+	link.href = item.getMetadata('url');
+	} else {
+	link.href = window.URL.createObjectURL(item.file);
+	link.download = item.file.name;
+	}
+	link.appendChild(icon);
+	return link;
 }
