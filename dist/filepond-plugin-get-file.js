25bf8a6201267d0288ec08c3f801c692be3cc8ef
Anastasia Belozerceva <belozerceva.a@ya.ru>
2021-12-02 12:37:38 +0300

    Build project

--- dist/filepond-plugin-get-file.js
+++ dist/filepond-plugin-get-file.js
@@ -27,42 +27,32 @@
     allowDownloadByUrl
   ) => {
     const info = el.querySelector('.filepond--file-info-main'),
-      downloadIcon = getDownloadIcon(labelButtonDownload);
+      downloadIcon = getDownloadIcon(
+        labelButtonDownload,
+        item,
+        allowDownloadByUrl
+      );
     info.prepend(downloadIcon);
-    downloadIcon.addEventListener('click', () =>
-      downloadFile(item, allowDownloadByUrl)
-    );
   };
   /**
    * Generates the download icon
    */
 
-  const getDownloadIcon = (labelButtonDownload) => {
+  const getDownloadIcon = (labelButtonDownload, item, allowDownloadByUrl) => {
+    let link = document.createElement('a');
     let icon = document.createElement('span');
     icon.className = 'filepond--download-icon';
     icon.title = labelButtonDownload;
-    return icon;
-  };
-  /**
-   * Triggers the actual download of the uploaded file
-   */
 
-  const downloadFile = (item, allowDownloadByUrl) => {
-    // if client want to download file from remote server
     if (allowDownloadByUrl && item.getMetadata('url')) {
-      location.href = item.getMetadata('url'); // full path to remote server is stored in metadata with key 'url'
+      link.href = item.getMetadata('url');
     } else {
-      // create a temporary hyperlink to force the browser to download the file
-      const a = document.createElement('a');
-      const url = window.URL.createObjectURL(item.file);
-      document.body.appendChild(a);
-      a.style.display = 'none';
-      a.href = url;
-      a.download = item.file.name;
-      a.click();
-      window.URL.revokeObjectURL(url);
-      a.remove();
+      link.href = window.URL.createObjectURL(item.file);
+      link.download = item.file.name;
     }
+
+    link.appendChild(icon);
+    return link;
   };
 
   /**
